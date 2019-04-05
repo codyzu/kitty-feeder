fb1cbf037be0967e433090662b39d12cb92807a8
Cody Zuschlag <cody.zuschlag@gmail.com>
2019-04-05 19:23:49 +0200

    add: react-daypicker

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Container, Navbar, NavbarBrand, Row, Col, Card, CardHeader, CardBody, Form, FormGroup, Label, Input} from 'reactstrap';
+import DayPicker from 'react-day-picker';
+import 'react-day-picker/lib/style.css'; // eslint-disable-line import/no-unassigned-import
 // Import logo from './logo.svg';
 // Import './App.css'; // eslint-disable-line import/no-unassigned-import
 
 const App = () => {
+  const [selectedDay, setSelectedDay] = useState(null);
+
+  function handleClickedDay(day) {
+    setSelectedDay(day);
+  }
+
   return (
     <>
       <Container>
-        <Navbar>
+        <Navbar light color="light">
           <NavbarBrand>Cat Feeder</NavbarBrand>
         </Navbar>
       </Container>
       <Container>
-        <Row>
+        <Row className="mt-4">
           <Col><h1>Schedule a feeding</h1></Col>
         </Row>
         <Row>
@@ -24,6 +32,24 @@ const App = () => {
                   <FormGroup>
                     <Label for="feed-date">Date</Label>
                     <Input required type="date" id="feed-date" name="feed-date" pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"/>
+                  </FormGroup>
+                </Form>
+              </CardBody>
+              <CardBody>
+                <Form>
+                  <FormGroup row>
+                    <Label className="col-sm-12" for="feed-date2">Date</Label>
+                    <DayPicker
+                      disabledDays={[{before: new Date()}]}
+                      selectedDays={selectedDay}
+                      renderDay={(day, modifiers) => {
+                        const color = modifiers.today ? 'text-success' :
+                          modifiers.disabled ? 'text-muted' :
+                            '';
+                        return <span className={color}>{day.getDate()}</span>;
+                      }}
+                      onDayClick={handleClickedDay}
+                    />
                   </FormGroup>
                 </Form>
               </CardBody>
