2c5870cec10a4ebee238d9501b974210576e02f0
Cody Zuschlag <cody.zuschlag@gmail.com>
2019-04-08 22:36:09 +0200

    chore: improve build

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import firebase from './firebase-app';
+
+afterAll(() => {
+  console.log('Cleaning up...');
+  return firebase.app().delete();
+});
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
